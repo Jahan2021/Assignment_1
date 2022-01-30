87a9ab70c16b192372f44bca56e139436046001f
Munima <Munima@10.0.0.215>
2022-01-30 11:27:46 -0700

    player updated

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -6,4 +6,14 @@ public class Player {
 	 * This class represent each player record in the Database
 	 * It is basically a model class for each record in the txt file
 	 */
+	
+	private String name;
+	private int wins;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
